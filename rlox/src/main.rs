dd0363e1815d8692f73775cfb9a02392e0df0ff3
André Nogueira <andre@nog.dev>
2022-01-16 18:44:12 -0300

    making tokens

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,4 +1,9 @@
 use std::env;
+use std::fs;
+use std::io::Write;
+use std::io::{stdin, stdout};
+
+use regex::Regex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,8 +19,43 @@ fn main() {
 
 fn run_file(path: &String){
     println!("Running file \"{path}\"");
+    let content_string = fs::read_to_string(path);
+    let content_string = match content_string {
+        Ok(content) => content,
+        Err(error) => panic!("Problem opening the file: {:?}", error)
+    };
+
+    run(&content_string);
 }
 
 fn run_prompt() {
-    println!("Running prompt...");
+
+    let re = Regex::new(r"\s+").unwrap();
+
+    loop {
+        let mut s = String::new();
+        print!("> ");
+
+        match stdout().flush() {
+            Ok(_all_good) => {},
+            Err(error) => panic!("Failed to flush stdout: {:?}", error)
+        };
+        
+        match stdin().read_line(&mut s) {
+            Ok(_goes_into_input_above) => {},
+            Err(error) => panic!("Failed interpreting input: {:?}", error)
+        }
+
+        let string = &s.trim().to_string();
+        let result = re.replace_all(string, " ");
+        run(&result.to_string());
+    }
+}
+
+fn run(content: &String){
+    println!("Tokens:");
+    let tokens: Vec<&str> = content.split(" ").collect();
+    for token in tokens {
+        println!("\t- {token}");
+    }
 }
