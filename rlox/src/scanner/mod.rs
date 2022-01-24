7b0e41fc675823dafe81a765c6227f8e94e3e2e9
André Nogueira <andre@nog.dev>
2022-01-24 00:31:00 -0300

    identifiers working

--- rlox/src/scanner/mod.rs
+++ rlox/src/scanner/mod.rs
@@ -2,6 +2,7 @@ pub mod token;
 mod token_type;
 
 use crate::lox::{Lox, LoxInterpreter};
+use std::collections::HashMap;
 use token::Token;
 use token_type::TokenType;
 
@@ -12,6 +13,7 @@ pub struct Scanner<'a> {
     current: u8,
     line: u8,
     lox_instance: &'a mut Lox,
+    keywords: HashMap<String, TokenType>,
 }
 
 pub trait ScannerActions {
@@ -24,6 +26,12 @@ pub trait ScannerActions {
     fn add_token_with_literal(&mut self, tokentype: TokenType, literal: String);
     fn match_next(&mut self, expected: char) -> bool;
     fn peek(&self) -> char;
+    fn peek_next(&self) -> char;
+    fn is_digit(&self, c: char) -> bool;
+    fn number(&mut self);
+    fn identifier(&mut self);
+    fn is_alpha(&self, c: char) -> bool;
+    fn is_alpha_numeric(&self, c: char) -> bool;
 }
 
 impl ScannerActions for Scanner<'_> {
@@ -95,10 +103,66 @@ impl ScannerActions for Scanner<'_> {
             ' ' | '\r' | '\t' => (),
             '\n' => self.line += 1,
             '"' => self.string(),
-            _ => self
-                .lox_instance
-                .error(self.line, &String::from("Unexpected character")),
+            _ => {
+                if self.is_digit(c) {
+                    self.number()
+                } else if self.is_alpha(c) {
+                    self.identifier()
+                } else {
+                    self.lox_instance
+                        .error(self.line, &String::from("Unexpected character"));
+                }
+            }
         }
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let text: String = self
+            .source
+            .chars()
+            .skip(self.start.into())
+            .take((self.current - self.start).into())
+            .collect();
+
+        let tokentype: TokenType = match self.keywords.remove(&text) {
+            Some(value) => value,
+            None => TokenType::Identifier,
+        };
+
+        let to_re_insert = tokentype.clone();
+        self.keywords.insert(text.to_string(), to_re_insert);
+
+        self.add_token(tokentype);
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        // look for fractional part
+        let next_peek = self.peek_next();
+        if self.peek() == '.' && self.is_digit(next_peek) {
+            // Consume the "."
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let final_number_text: String = self
+            .source
+            .chars()
+            .skip(self.start.into())
+            .take((self.current - self.start).into())
+            .collect();
+
+        self.add_token_with_literal(TokenType::Number, final_number_text)
     }
 
     fn string(&mut self) {
@@ -153,6 +217,26 @@ impl ScannerActions for Scanner<'_> {
         return self.source.chars().nth(self.current.into()).unwrap();
     }
 
+    fn peek_next(&self) -> char {
+        if usize::from(self.current + 1) >= self.source.len() {
+            return '\0';
+        }
+
+        return self.source.chars().nth((self.current + 1).into()).unwrap();
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_alpha_numeric(&self, c: char) -> bool {
+        self.is_alpha(c) || self.is_digit(c)
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        return c >= '0' && c <= '9';
+    }
+
     fn advance(&mut self) -> char {
         let to_return = match self.source.chars().nth(self.current.into()) {
             Some(value) => value,
@@ -203,5 +287,23 @@ pub fn create_scanner(lox_instance: &mut Lox, content: String) -> Scanner {
         current: 0,
         line: 1,
         lox_instance,
+        keywords: HashMap::from([
+            ("and".to_string(), TokenType::And),
+            ("class".to_string(), TokenType::Class),
+            ("else".to_string(), TokenType::Else),
+            ("false".to_string(), TokenType::False),
+            ("for".to_string(), TokenType::For),
+            ("fun".to_string(), TokenType::Fun),
+            ("if".to_string(), TokenType::If),
+            ("nil".to_string(), TokenType::Nil),
+            ("or".to_string(), TokenType::Or),
+            ("print".to_string(), TokenType::Print),
+            ("return".to_string(), TokenType::Return),
+            ("super".to_string(), TokenType::Super),
+            ("this".to_string(), TokenType::This),
+            ("true".to_string(), TokenType::True),
+            ("var".to_string(), TokenType::Var),
+            ("while".to_string(), TokenType::While),
+        ]),
     }
 }
